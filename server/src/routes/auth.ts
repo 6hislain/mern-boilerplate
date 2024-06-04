4ef1897474fb8a14f8c645915b5cc249a48657b3
6hislain <ghislainmike80@gmail.com>
2024-06-04 14:04:16 +0200

    middleware

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,37 +2,42 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User, { IUser } from "../models/user";
 import express, { Request, Response } from "express";
+import { guestMiddleware } from "../utils/middleware";
 
 const router = express.Router();
 
-router.post("/register", async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { name, email, password } = req.body;
+router.post(
+  "/register",
+  guestMiddleware,
+  async (req: Request, res: Response) => {
+    try {
+      const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      res.status(400).json({ message: "User already exists" });
-      return;
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        res.status(400).json({ message: "User already exists" });
+        return;
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const newUser: IUser = new User({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      await newUser.save();
+
+      res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+      console.error("Error registering user:", error);
+      res.status(500).json({ message: "Internal server error" });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser: IUser = new User({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    await newUser.save();
-
-    res.status(201).json({ message: "User registered successfully" });
-  } catch (error) {
-    console.error("Error registering user:", error);
-    res.status(500).json({ message: "Internal server error" });
   }
-});
+);
 
-router.post("/login", async (req: Request, res: Response): Promise<void> => {
+router.post("/login", guestMiddleware, async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
 
