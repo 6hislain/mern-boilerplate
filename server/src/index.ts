4603ed2e95d389e191c90565f5af04c82903b3a8
6hislain <ghislainmike80@gmail.com>
2024-06-04 14:12:24 +0200

    static files

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ connect(process.env.DB_URL || "");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/static", express.static(path.join(__dirname, "../public")));
 
 app.use(defaultRoutes);
 app.use(authRoutes);
